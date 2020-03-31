568a5a4a88e96db8140d1cb77600ba7b93f3156c
neethu-ms <msneethu2989@gmail.com>
2020-03-31 14:26:59 +0000

    Find and update store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,7 +1,21 @@
 require_relative '../setup'
 require_relative './exercise_1'
-
+require './lib/store.rb'
 puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+class StoreUpdate
+  attr_accessor :store1, :store2
+  def get_store
+     @store1 = Store.find_by(id:1)
+     @store2 = Store.find_by(id:2)
+     @store1.name = "Daniel"
+     @store1.save
+  end
+end
+
+store_update = StoreUpdate.new
+store_update.get_store
+p store_update.store1
+p store_update.store2
