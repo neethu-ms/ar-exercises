e68c9d0d15bc469a6665bfc13b3ecfb617d3a9c7
neethu-ms <msneethu2989@gmail.com>
2020-03-31 14:14:16 +0000

    Refactor code

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,6 +5,9 @@ puts "Exercise 1"
 puts "----------"
 
  #Your code goes below here ...
+class Stores_Details
+
+def create_stores
 Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true,
 womens_apparel:true);
 
@@ -15,3 +18,8 @@ Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true,
 womens_apparel:false);
 
 puts Store.count
+end
+end
+
+stores = Stores_Details.new
+stores.create_stores 
