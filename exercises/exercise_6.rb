4be4c6fcbb7f0704f9ab33e035b2f7816b02c71a
neethu-ms <msneethu2989@gmail.com>
2020-03-31 17:57:59 +0000

    Add One-to-many association

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -4,8 +4,19 @@ require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
+require './lib/employee.rb'
 
 puts "Exercise 6"
 puts "----------"
 
+
+  @store1 = Store.all.first
+  @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+  @store1.employees.create(first_name: "Alice", last_name: "Vincent", hourly_rate: 50)
+  @store1.employees.create(first_name: "Michael", last_name: "George", hourly_rate: 40)
+  @store1 = Store.all.second
+  @store1.employees.create(first_name: "Bob", last_name: "George", hourly_rate: 40)
+  @store1.employees.create(first_name: "Francis", last_name: "Paul", hourly_rate: 30)
+
 # Your code goes here ...
+
