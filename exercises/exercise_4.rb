3fa96f3971f0fb06cef1fc92950cb505f6c2963f
neethu-ms <msneethu2989@gmail.com>
2020-03-31 16:44:24 +0000

    Create and fetch records based on condition

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,33 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+class Stores_Details
+  attr_accessor :mens_stores, :women_stores
+def create_stores
+ 
+  Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false,
+  womens_apparel:true);
+  
+  Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true,
+  womens_apparel:false);
+  
+  Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true,
+  womens_apparel:true);
+
+end
+
+def get_stores
+  @mens_stores = Store.where(mens_apparel:true)
+  mens_stores.each{|store| puts "#{store.name} #{store.annual_revenue} "  }
+  
+  @women_stores = Store.where("womens_apparel = true and annual_revenue < 1000000")
+  women_stores.each{|store| puts "#{store.name} #{store.annual_revenue}"} 
+  puts Store.count
+  end
+
+  end
+  
+  stores = Stores_Details.new
+  stores.create_stores 
+  stores.get_stores
+  
