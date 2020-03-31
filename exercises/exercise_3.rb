d7b1081e88d3ff528da107627fe47944ca2700a4
neethu-ms <msneethu2989@gmail.com>
2020-03-31 16:42:06 +0000

    Delete record from stores

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,14 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+class StoreDetails
+  attr_accessor :store3
+  def delete_store_id
+     @store3 = Store.find_by(id:3)
+     @store3.destroy
+     puts Store.count          
+  end
+end
+
+store_details = StoreDetails.new
+store_details.delete_store_id
